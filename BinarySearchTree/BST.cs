41d229ba928a543a23e2d45fbccc01f69d56f0d2
Jake Keena <jacobkeena@users.noreply.github.com>
2019-04-21 12:01:53 -0400

    Initial Progress

---
+++ BinarySearchTree/BST.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BST
+    {
+        public Node root;
+        public BST()
+        {
+            root = null;
+        }
+
+        public void AddNumber(int i)
+        {
+            Node newNode = new Node();
+            newNode.data = i;
+
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.data)
+                    {
+                        current = current.Left;
+                        if (current == null)
+                        {
+                            parent.Left = newNode;
+
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.Right;
+                        if (current == null)
+                        {
+                            parent.Right = newNode;
+                            break;
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+    }
+}
--- BinarySearchTree/MainWindow.xaml.cs
+++ BinarySearchTree/MainWindow.xaml.cs
@@ -24,5 +24,77 @@ namespace BinarySearchTree
         {
             InitializeComponent();
         }
+
+        
+
+        private void GenerateNumbers_Clicked(object sender, RoutedEventArgs e)
+        {
+            BST bst = new BST();
+            Random rnd = new Random();
+            int totalNumbers = 0;
+            int minRange = 0;
+            int maxRange = 0;
+
+            if (string.IsNullOrEmpty(minBox.Text) || string.IsNullOrEmpty(maxBox.Text) || string.IsNullOrEmpty(numberAmount.Text))
+            {
+                MessageBox.Show("Each box must have a value", "Error", MessageBoxButton.OK);
+                return;
+            }
+
+
+            try
+            {
+                totalNumbers = int.Parse(numberAmount.Text);
+                minRange = int.Parse(minBox.Text);
+                maxRange = int.Parse(maxBox.Text);
+                if (totalNumbers < 1)
+                {
+                    MessageBox.Show("Please enter a whole number greater than 0 and less than 1,000,000,000.", "Error", MessageBoxButton.OK);
+                    return;
+                }
+                //selectionRange = totalNumbers * 2; // I need a pool of numbers larger than the amount inside the tree
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Amount a whole number greater than 0 and less than 1,000,000,000. ", "Error", MessageBoxButton.OK);
+                return;
+            }
+            finally
+            {
+                for (uint i = 0; i < totalNumbers; i++)
+                {
+                    //randomly generate numbers within the selection range 
+                    bst.AddNumber(rnd.Next(minRange, maxRange));
+                }
+
+                successMessage.Text = $"Binary tree successfully created with {totalNumbers} random values inserted";
+                WriteToDisplayBox(bst.root);
+
+            }
+        }
+
+
+        public void WriteToDisplayBox(Node node)
+        {
+            displayBSTBox.Text = "";
+            if(node.Left == null)
+            {
+                displayBSTBox.Text += node.data.ToString() + " ";
+            }
+            else
+            {
+                WriteToDisplayBox(node.Left);
+            }
+            if(node.Right == null)
+            {
+                displayBSTBox.Text += node.data.ToString() + " ";
+            }
+            else
+            {
+                WriteToDisplayBox(node.Right);
+            }
+        }
     }
+
+
 }
